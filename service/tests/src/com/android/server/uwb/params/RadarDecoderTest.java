c982426488e351320ee15bcd1085dac3cff9c12a
Bob Wang <ziyiw@google.com>
2026-07-09 11:31:45 -0700

    [Radar] Handle vendor optional tlv tags more gracefully.
    Test: compile
    Bug: 531727207
    Flag: EXEMPT BUGFIX
    Change-Id: Ie074c78e50b1a2dba3473eed5f60b0320a8e5daf
    (cherry picked from commit e07fdc692228edaef4268583e127bbc87ec35b05)
    Crs-Fixed: 4598525

--- service/tests/src/com/android/server/uwb/params/RadarDecoderTest.java
+++ service/tests/src/com/android/server/uwb/params/RadarDecoderTest.java
@@ -26,10 +26,12 @@ import androidx.test.runner.AndroidJUnit4;
 import com.android.server.uwb.UwbInjector;
 import com.android.server.uwb.util.UwbUtil;
 
+import com.google.uwb.support.fira.FiraParams;
 import com.google.uwb.support.fira.FiraProtocolVersion;
 import com.google.uwb.support.radar.RadarData;
 import com.google.uwb.support.radar.RadarParams;
 import com.google.uwb.support.radar.RadarParams.RadarCapabilityFlag;
+import com.google.uwb.support.radar.RadarRangingStartedParams;
 import com.google.uwb.support.radar.RadarSpecificationParams;
 
 import org.junit.Before;
@@ -52,6 +54,43 @@ public class RadarDecoderTest {
     public static final int TEST_RADAR_SPECIFICATION_TLV_NUM_PARAMS = 1;
     private static final EnumSet<RadarCapabilityFlag> RADAR_CAPABILITIES =
             EnumSet.of(RadarCapabilityFlag.HAS_RADAR_SWEEP_SAMPLES_SUPPORT);
+
+    private static final byte[] TEST_RADAR_RANGING_STARTED_TLV_DATA =
+            UwbUtil.getByteArray(
+                    "000764000000280010"
+                            + "010110"
+                            + "020105"
+                            + "0302ffff"
+                            + "040103"
+                            + "050109"
+                            + "06015a"
+                            + "070163"
+                            + "080100"
+                            + "090101"
+                            + "0a02e803"
+                            + "0b0100"
+                            + "a0020801"
+                            + "a1010c"
+                            + "a20118"
+                            + "a3011e");
+    private static final int TEST_RADAR_RANGING_STARTED_TLV_NUM_PARAMS = 16;
+
+    private static final byte[] TEST_RADAR_RANGING_STARTED_NO_OPTIONAL_TLV_DATA =
+            UwbUtil.getByteArray(
+                    "000764000000280010"
+                            + "010110"
+                            + "020105"
+                            + "0302ffff"
+                            + "040103"
+                            + "050109"
+                            + "06015a"
+                            + "070163"
+                            + "080100"
+                            + "090101"
+                            + "0a02e803"
+                            + "0b0100");
+    private static final int TEST_RADAR_RANGING_STARTED_NO_OPTIONAL_TLV_NUM_PARAMS = 12;
+
     private final RadarDecoder mRadarDecoder = new RadarDecoder();
     @Mock
     private UwbInjector mUwbInjector;
@@ -64,6 +103,38 @@ public class RadarDecoderTest {
     public static void verifyRadarSpecification(RadarSpecificationParams radarSpecificationParams) {
         assertThat(radarSpecificationParams).isNotNull();
         assertThat(radarSpecificationParams.getRadarCapabilities()).isEqualTo(RADAR_CAPABILITIES);
+    }
+
+    public static void verifyRadarRangingStarted(
+            RadarRangingStartedParams params, boolean hasOptional) {
+        assertThat(params).isNotNull();
+        assertThat(params.getBurstPeriod()).isEqualTo(100);
+        assertThat(params.getSweepPeriod()).isEqualTo(40);
+        assertThat(params.getSweepsPerBurst()).isEqualTo(16);
+        assertThat(params.getSamplesPerSweep()).isEqualTo(16);
+        assertThat(params.getChannelNumber()).isEqualTo(FiraParams.UWB_CHANNEL_5);
+        assertThat(params.getSweepOffset()).isEqualTo(-1);
+        assertThat(params.getRframeConfig()).isEqualTo(FiraParams.RFRAME_CONFIG_SP3);
+        assertThat(params.getPreambleDuration())
+                .isEqualTo(RadarParams.PREAMBLE_DURATION_T16384_SYMBOLS);
+        assertThat(params.getPreambleCodeIndex()).isEqualTo(90);
+        assertThat(params.getSessionPriority()).isEqualTo(99);
+        assertThat(params.getBitsPerSample()).isEqualTo(RadarParams.BITS_PER_SAMPLES_32);
+        assertThat(params.getPrfMode()).isEqualTo(FiraParams.PRF_MODE_HPRF);
+        assertThat(params.getNumberOfBursts()).isEqualTo(1000);
+        assertThat(params.getRadarDataType())
+                .isEqualTo(RadarParams.RADAR_DATA_TYPE_RADAR_SWEEP_SAMPLES);
+        if (hasOptional) {
+            assertThat(params.getAntennaBitmap()).isEqualTo(0x0108);
+            assertThat(params.getGpioBitmap()).isEqualTo(0x0C);
+            assertThat(params.getTxPower()).isEqualTo(24);
+            assertThat(params.getRxGain()).isEqualTo(30);
+        } else {
+            assertThat(params.getAntennaBitmap()).isEqualTo(0);
+            assertThat(params.getGpioBitmap()).isEqualTo(0);
+            assertThat(params.getTxPower()).isEqualTo(0);
+            assertThat(params.getRxGain()).isEqualTo(0);
+        }
     }
 
     @Test
@@ -101,5 +172,33 @@ public class RadarDecoderTest {
                         .getParams(tlvDecoderBuffer, RadarSpecificationParams.class,
                                  PROTOCOL_VERSION_DUMMY);
         verifyRadarSpecification(radarSpecificationParams);
+    }
+
+    @Test
+    public void testGetRadarRangingStarted() throws Exception {
+        TlvDecoderBuffer tlvDecoderBuffer =
+                new TlvDecoderBuffer(
+                        TEST_RADAR_RANGING_STARTED_TLV_DATA,
+                        TEST_RADAR_RANGING_STARTED_TLV_NUM_PARAMS);
+        assertThat(tlvDecoderBuffer.parse()).isTrue();
+
+        RadarRangingStartedParams params =
+                mRadarDecoder.getParams(tlvDecoderBuffer, RadarRangingStartedParams.class,
+                        PROTOCOL_VERSION_DUMMY);
+        verifyRadarRangingStarted(params, true);
+    }
+
+    @Test
+    public void testGetRadarRangingStarted_noOptional() throws Exception {
+        TlvDecoderBuffer tlvDecoderBuffer =
+                new TlvDecoderBuffer(
+                        TEST_RADAR_RANGING_STARTED_NO_OPTIONAL_TLV_DATA,
+                        TEST_RADAR_RANGING_STARTED_NO_OPTIONAL_TLV_NUM_PARAMS);
+        assertThat(tlvDecoderBuffer.parse()).isTrue();
+
+        RadarRangingStartedParams params =
+                mRadarDecoder.getParams(tlvDecoderBuffer, RadarRangingStartedParams.class,
+                        PROTOCOL_VERSION_DUMMY);
+        verifyRadarRangingStarted(params, false);
     }
 }
